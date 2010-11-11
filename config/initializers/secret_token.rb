31299dcd204aac5a4ed64ce8938a7df100c4f267
René Sprotte <rene.sprotte@provideal.net>
2010-11-11 11:09:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MmCms::Application.config.secret_token = '29a12079c550150fed084542205ce5e4e3a8159c644f97894b778e40b08e69924ac082bb1e95c52c244a7b72208b4f102bc5b8314241eebc75912935a3bf9ee9'
