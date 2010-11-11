9ff14c013eeed4c0734246efbbe3baa8acfd6a9b
René Sprotte <rene.sprotte@provideal.net>
2010-11-11 11:45:56 +0100

    Added user (empty) user model und basic sprec setup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,11 +17,11 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  # Mongodb does not support transactions, so we can't use
+  # transactional fixture and we need to clean the data manually.
+  config.after :suite do
+    Mongoid.master.collections.select do |collection|
+      collection.name !~ /system/
+    end.each(&:drop)
+  end
 end
