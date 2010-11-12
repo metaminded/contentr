ad5b201ada3f901589f3d6d9e7a65046f97dd4e2
René Sprotte <rene.sprotte@provideal.net>
2010-11-12 21:04:47 +0100

    Layout support for pages

--- app/models/mm_cms/page.rb
+++ app/models/mm_cms/page.rb
@@ -1,6 +1,25 @@
-class MmCms::Page < MmCms::Item
+module MmCms
+  class Page < Item
 
-  # Fields
-  # ...
+    # Fields
+    field :layout, :default => 'default'
 
+    def layout
+      self.read_attribute(:layout) || 'default'
+    end
+
+    # Liquid
+    def to_liquid
+      PageLiquidProxy.new(self)
+    end
+  end
+
+  ##
+  # TBD
+  #
+  class PageLiquidProxy < ItemLiquidProxy
+    def layout
+      @item.layout
+    end
+  end
 end
