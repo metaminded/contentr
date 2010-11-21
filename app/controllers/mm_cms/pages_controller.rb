a19ce5b40672c5f50eb214059f790f4afa68aa42
René Sprotte <rene.sprotte@provideal.net>
2010-11-21 20:55:46 +0100

    Better names.

--- app/controllers/mm_cms/pages_controller.rb
+++ app/controllers/mm_cms/pages_controller.rb
@@ -12,17 +12,17 @@ class MmCms::PagesController < MmCms::ApplicationController
     path = params[:path]
     return redirect_to cms_url(:path => @site.default_page_path) if path.blank?
 
-    @item = MmCms::Item.find_by_path(path)
-    @item.present? ? render_item : render_item_not_found
+    @item = MmCms::Page.find_by_path(path)
+    @item.present? ? render_page : render_page_not_found
   end
 
 protected
 
-  def render_item
+  def render_page
     render_liquid_template
   end
 
-  def render_item_not_found
+  def render_page_not_found
     render :text => "No such page", :status => 404
   end
 
@@ -43,6 +43,43 @@ protected
   #
   def setup_liquid
     Liquid::Template.file_system = Liquid::LocalFileSystem.new(@site.theme_name)
+  end
+
+  ##
+  # Renders a Liquid template (for the current action) together
+  # with a Liquid layout.
+  #
+  def render_liquid_template(options = {})
+    # Merge the options hash with some useful defaults
+    options = {
+      :layout    => @item.layout,
+      :template  => @item.template,
+      :assigns   => {},
+      :registers => {}
+    }.merge(options)
+
+    # override layout by request if available
+    options[:layout] = params[:__layout] if params[:__layout].present?
+
+    # Global assignments that are always available
+    options[:assigns]['request_params'] = request.params
+    options[:assigns]['item']           = @item
+    options[:assigns]['site']           = @site
+
+    # Global registers that are always available
+    options[:registers]['controller'] = self
+
+    # Load the template identified by the given template name.
+    template_file    = get_liquid_template(options[:template])
+    template_content = template_file.render!(options[:assigns], { :registers => options[:registers] })
+    options[:assigns]['content_for_layout'] = template_content
+
+    # Load the Liquid layout file.
+    layout_file      = get_liquid_template(options[:layout], true)
+    layout_content   = layout_file.render!(options[:assigns], { :registers => options[:registers] })
+
+    # Finally write out the result.
+    render :text => layout_content
   end
 
   ##
@@ -83,43 +120,6 @@ protected
     raise "No such template file #{liquid_template}" unless File.exists?(liquid_template)
 
     File.read(liquid_template)
-  end
-
-  ##
-  # Renders a Liquid template (for the current action) together
-  # with a Liquid layout.
-  #
-  def render_liquid_template(options = {})
-    # Merge the options hash with some useful defaults
-    options = {
-      :layout    => @item.layout,
-      :template  => @item.template,
-      :assigns   => {},
-      :registers => {}
-    }.merge(options)
-
-    # override layout by request if available
-    options[:layout] = params[:__layout] if params[:__layout].present?
-
-    # Global assignments that are always available
-    options[:assigns]['request_params'] = request.params
-    options[:assigns]['item']           = @item
-    options[:assigns]['site']           = @site
-
-    # Global registers that are always available
-    options[:registers]['controller'] = self
-
-    # Load the template identified by the given template name.
-    template_file    = get_liquid_template(options[:template])
-    template_content = template_file.render!(options[:assigns], { :registers => options[:registers] })
-    options[:assigns]['content_for_layout'] = template_content
-
-    # Load the Liquid layout file.
-    layout_file      = get_liquid_template(options[:layout], true)
-    layout_content   = layout_file.render!(options[:assigns], { :registers => options[:registers] })
-
-    # Finally write out the result.
-    render :text => layout_content
   end
 
 end
