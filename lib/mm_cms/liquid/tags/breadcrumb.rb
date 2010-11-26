60d4783bd20fb0b085c7efb8499a786eb835e216
René Sprotte <rene.sprotte@provideal.net>
2010-11-26 13:26:55 +0100

    Breadcrumbs anyone

---
+++ lib/mm_cms/liquid/tags/breadcrumb.rb
@@ -0,0 +1,35 @@
+class MmCms::Liquid::Tags::Breadcrumb < ::Liquid::Block
+
+  Syntax = /for\s+(#{::Liquid::QuotedFragment}+)/
+
+  def initialize(tag_name, markup, tokens)
+    if markup =~ Syntax
+      @page_name = $1
+    else
+      @page_name = nil
+    end
+
+    super
+  end
+
+  def render(context)
+    current_page = context.registers["page"]
+    pages = []
+    if (current_page.present?)
+      pages = current_page.ancestors_and_self.to_a
+    end
+
+    result = []
+    context.stack do
+      pages.each do |p|
+        context['page'] = p
+        result << render_all(@nodelist, context)
+      end
+    end
+
+    result
+  end
+
+end
+
+::Liquid::Template.register_tag('breadcrumb', MmCms::Liquid::Tags::Breadcrumb)
