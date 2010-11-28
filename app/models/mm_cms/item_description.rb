84a3b58ca7dd4ba38b13624b39df63aecdb1712a
René Sprotte <rene.sprotte@provideal.net>
2010-11-28 18:43:46 +0100

    Fixed syntax.

--- app/models/mm_cms/item_description.rb
+++ app/models/mm_cms/item_description.rb
@@ -1,35 +1,35 @@
 module MmCms
   class ItemDescription
-    
+
     def init(name, descr)
       raise "Name must be simple" unless /^[a-z_A-Z0-9]+$/.match(name)
       raise "ItemDescription must specify 'fields'" unless description['fields']
       @name        = name
-      @label       = descr['label'] || raise "ItemDescription must specify a 'label'"
-      @description = descr['description'] || raise "ItemDescription must specify a 'description'"
+      @label       = descr['label'] || raise("ItemDescription must specify a 'label'")
+      @description = descr['description'] || raise("ItemDescription must specify a 'description'")
       @fields      = descr['fields'].map { |nam,d| ItemDataDescription.new(nam, d) }
     end
 
     def self.read_models
-      mfiles = Dir.glob("#{RAILS_ROOT}/public/mm_cms/model/*.yml")
-      @@models = mfiles.map do |mf| 
-        YAML.load_file(mf) 
-      end.inject({}) do |p,h| 
-        p.merge h 
+      mfiles = Dir.glob("#{Rails.root}/public/mm_cms/model/*.yml")
+      @@models = mfiles.map do |mf|
+        YAML.load_file(mf)
+      end.inject({}) do |p,h|
+        p.merge h
       end.map |k,v|
         ItemDescription.new(k, v)
       end
     end
   end
-  
-  class ItemDataDescription 
+
+  class ItemDataDescription
     def init(name, descr)
       raise "Name must be simple" unless /^[a-z_A-Z0-9]+$/.match(name)
       @name        = name
-      @label       = descr['label'] || raise "ItemDataDescription must specify a 'label'"
-      @description = descr['description'] || raise "ItemDataDescription must specify a 'description'"
-      @type        = descr['type'] || raise "ItemDataDescription must specify a 'label'"
-      raise "Wrong type '#{@type}'" unless %w{BigDecimal Boolean Date DateTime Float Integer String Time}.member? @type 
+      @label       = descr['label'] || raise("ItemDataDescription must specify a 'label'")
+      @description = descr['description'] || raise("ItemDataDescription must specify a 'description'")
+      @type        = descr['type'] || raise("ItemDataDescription must specify a 'label'")
+      raise "Wrong type '#{@type}'" unless %w{BigDecimal Boolean Date DateTime Float Integer String Time}.member? @type
       @required    = descr['required'] and %w{yes ja true 1 please yup yeah}.member? descr['required'].downcase
       @format      = descr['format'] ? Regexp.new(descr['format']) : nil
       @min_value   = descr['min_value']
