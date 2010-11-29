ce0796c7ccc2f0e07013c1c3942e2d78e986c859
René Sprotte <rene.sprotte@provideal.net>
2010-11-29 11:18:52 +0100

    Improved the include tag.

--- lib/mm_cms/liquid/tags/include.rb
+++ lib/mm_cms/liquid/tags/include.rb
@@ -1,17 +1,38 @@
 module MmCms::Liquid::Tags
-  class Include < ::Liquid::Include
+  class Include < ::Liquid::Tag
+
+    Syntax = /(#{::Liquid::QuotedFragment}+)/
 
     def initialize(tag_name, markup, tokens)
-      super(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @template_name = $1
+        @attributes    = {}
+
+        markup.scan(::Liquid::TagAttributes) do |key, value|
+          @attributes[key] = value
+        end
+      else
+        raise SyntaxError.new("Error in tag 'include' - Valid syntax: include '[template]'")
+      end
+
+      super
     end
 
     def render(context)
       site    = context.registers['site']
       request = context.registers['request']
-      liquid  = MmCms::Liquid::RenderEngine.new(site.themes_path, site.theme_name, request)
 
-      partial = liquid.parse_template(@template_name)
-      partial.render(context)
+      if (site and request)
+        liquid  = MmCms::Liquid::RenderEngine.new(site.themes_path, site.theme_name, request)
+        partial = liquid.parse_template(@template_name)
+        context.stack do
+          @attributes.each do |key, value|
+            context[key] = context[value]
+          end
+
+          partial.render(context)
+        end
+      end
     end
   end
 
