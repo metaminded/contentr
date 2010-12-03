9de4d9e0b2b291216f981866b4c414172385c2e3
René Sprotte <rene.sprotte@provideal.net>
2010-12-03 09:06:55 +0100

    Refactoring: Tree structure starts on page layer, not on item layer.

--- app/models/mm_cms/item.rb
+++ app/models/mm_cms/item.rb
@@ -6,14 +6,11 @@ module MmCms
   class Item
     # Includes
     include Mongoid::Document
-    include Mongoid::Tree
-    include Mongoid::Tree::Ordering
 
     # Fields & Relations
     field :name,        :type => String
     field :description, :type => String
     field :slug,        :type => String,  :index => true
-    field :path,        :type => String,  :index => true
     embeds_many :data, :class_name => 'MmCms::Data::Item' do
       def get(name)
         @target.select { |data| data.name == name }.first
@@ -30,27 +27,17 @@ module MmCms
 
     # Callbacks
     before_validation :generate_slug
-    before_destroy    :destroy_children
-    before_validation :rebuild_path
-    after_rearrange   :rebuild_path
-
-    def self.find_by_path(path)
-      MmCms::Item.where(:path => path).first
-    end
 
     def to_liquid
       MmCms::ItemLiquidProxy.new(self)
     end
-    
+
   protected
 
     def generate_slug
       self.slug = name.to_url
     end
 
-    def rebuild_path
-      self.path = self.ancestors_and_self.collect(&:slug).join('/')
-    end
   end
 
   ##
@@ -66,10 +53,6 @@ module MmCms
       MmCms::ItemDataLiquidProxy.new(@item.data)
     end
 
-    #def dataset
-    #  @item.data
-    #end
-
     def name
       @item.name
     end
@@ -80,18 +63,6 @@ module MmCms
 
     def slug
       @item.slug
-    end
-
-    def path
-      @item.path
-    end
-
-    def has_children
-      @item.children.count > 0
-    end
-
-    def children
-      @item.children.asc(:position)
     end
 
   end
--- app/models/mm_cms/page.rb
+++ app/models/mm_cms/page.rb
@@ -1,8 +1,13 @@
 module MmCms
   class Page < Item
+    # Includes
+    include Mongoid::Tree
+    include Mongoid::Tree::Ordering
+
     # Fields
-    field :layout,   :default => 'default'
-    field :template, :default => 'default'
+    field :path,     :type => String, :index => true
+    field :layout,   :type => String, :default => 'default'
+    field :template, :type => String, :default => 'default'
 
     # Protect attributes from mass assignment
     attr_accessible :layout, :template
@@ -11,10 +16,25 @@ module MmCms
     validates_presence_of :layout
     validates_presence_of :template
 
-    # Liquid
+    # Callbacks
+    before_destroy    :destroy_children
+    before_validation :rebuild_path
+    after_rearrange   :rebuild_path
+
+    def self.find_by_path(path)
+      MmCms::Page.where(:path => path).first
+    end
+
     def to_liquid
       PageLiquidProxy.new(self)
     end
+
+  protected
+
+    def rebuild_path
+      self.path = self.ancestors_and_self.collect(&:slug).join('/')
+    end
+
   end
 
   ##
@@ -27,6 +47,18 @@ module MmCms
 
     def template
       @item.template
+    end
+
+    def path
+      @item.path
+    end
+
+    def has_children
+      @item.children.count > 0
+    end
+
+    def children
+      @item.children.asc(:position)
     end
   end
 end
