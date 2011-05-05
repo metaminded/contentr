099e79ba15781ad31887ee6ff3ff6f4cd49317c8
René Sprotte <rene.sprotte@provideal.net>
2011-05-05 11:31:42 +0200

    Moved themes out uf /public

--- config/initializers/mm_cms.rb
+++ config/initializers/mm_cms.rb
@@ -2,7 +2,7 @@
 
 MmCms.setup do |config|
   config.site_name    = 'My Dummy Site'
-  config.themes_path  = File.join(Rails.root, 'public', 'mm_cms', 'themes')
+  config.themes_path  = File.join(Rails.root, 'cms', 'themes')
   config.theme_name   = 'default'
   config.default_page = 'home'
 end
