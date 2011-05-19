4d6f07c2e99d0acc9a34b24ff1910d72f4f19345
René Sprotte <rene.sprotte@provideal.net>
2011-05-19 13:31:00 +0200

    Beginings of a menu helper.

--- lib/contentr/view_helpers.rb
+++ lib/contentr/view_helpers.rb
@@ -8,12 +8,22 @@ module Contentr
 
       def area(area_name)
         if @_contentr_current_page.present? and area_name.present?
+          area_name = area_name.to_s
           paragraphs = @_contentr_current_page.paragraphs_for_area(area_name)
           content_tag(:div, 'data-contentr-area' => area_name) do
             paragraphs.collect do |p|
               template_name = p.class.to_s.tableize.singularize
               render(:partial => "contentr/paragraphs/#{template_name}", :locals => {:paragraph => p})
             end.join("").html_safe
+          end
+        end
+      end
+
+      def menu(menu_name)
+        if (menu_name.present?)
+          menu_name = menu_name.to_s
+          content_tag(:ul) do
+            content_tag(:li, 'TODO')
           end
         end
       end
