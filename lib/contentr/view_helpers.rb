6ee5927424b5d35c7609491f33b2f2809e836533
René Sprotte <rene.sprotte@provideal.net>
2011-05-20 11:57:06 +0200

    Finished menu helper

--- lib/contentr/view_helpers.rb
+++ lib/contentr/view_helpers.rb
@@ -42,46 +42,52 @@ module Contentr
 
         # walk the tree(s)
         page_or_pages.is_a?(Array) ? _page_tree(page_or_pages, current_page, options)
-                                   : _page_tree([page_or_pages], current_page, options)
+                                   : _page_tree(page_or_pages.children, current_page, options)
       end
 
       protected
 
       def _page_tree(nodes, current_page, options, depth = -1)
+        # Calc the current depth
         depth = depth + 1
-
+        # Render ...
         if nodes.present?
-          content_tag(:ul) do
+          content_tag(:ul, :class => "contentr #{options[:class] || 'menu'}") do
             nodes.collect do |p|
               # ignore hidden pages
               next if p.hidden?
               # ignore unpublished pages
               next unless p.published?
               # return if we reached max depth
-              puts "P: #{p.depth}"
-              puts "options: #{options[:depth]}"
-              puts "depth: #{depth}"
-              puts "-----"
+              return nil if depth > options[:depth]
 
-              return nil if p.depth > options[:depth]
-
-              # create li tag
-              content_tag(:li) do
+              # render li tag
+              li_options = {}
+              content_tag(:li, li_options) do
                 # default
                 s = ''
 
-                # the link
+                # the link url
                 link_url = p.is_link? ? url_for(p.controller_action_url_options)
                                       : File.join(Contentr.frontend_route_prefix, p.path)
                 link_options = {}
-                link_options[:class] = 'active' if p == current_page
+                # set the link css class
+                link_classes = []
+                # mark active (current) page
+                link_classes << 'active'  if p == current_page
+                # mark descendant pages of current page
+                link_classes << 'open' if current_page.descendant_of?(p)
+                # set the link classes
+                link_options[:class] = link_classes.join(' ') unless link_classes.empty?
+                # set link title
                 link_title = p.menu_title || p.name
-                s << link_to("[#{p.depth} - #{options[:depth]}] " + link_title, link_url, link_options)
+                # the link
+                s << link_to(link_title, link_url, link_options)
 
                 # the children
                 s << _page_tree(p.children, current_page, options, depth).to_s
 
-                # we are safe
+                # safe we are
                 s.html_safe
               end
             end.join("").html_safe
