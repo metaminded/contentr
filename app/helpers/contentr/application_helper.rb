6258415f054155f7783943035018cd47d1c75125
René Sprotte <rene.sprotte@provideal.net>
2011-05-28 20:43:26 +0200

    Tollbars :-)

--- app/helpers/contentr/application_helper.rb
+++ app/helpers/contentr/application_helper.rb
@@ -14,6 +14,7 @@ module Contentr
       if current_page.present? and area_name.present?
         area_name = area_name.to_s
         paragraphs = current_page.paragraphs_for_area(area_name)
+        editable = controller.contentr_editable?
 
         area_options = {}
         area_classes = []
@@ -24,18 +25,34 @@ module Contentr
         area_options['data-contentr-area'] = area_name
 
         content_tag(:div, area_options) do
-          paragraphs.collect do |p|
+          s = ''.html_safe
+
+          if editable
+            s << content_tag(:div, :class => 'contentr toolbar') do
+              'AREA TOOLBAR'
+            end
+          end
+
+          s << paragraphs.collect do |p|
             template_name = p.class.to_s.tableize.singularize
 
             paragraph_options = {}
             paragraph_classes = []
             paragraph_classes << 'contentr'
             paragraph_classes << 'paragraph'
-            paragraph_classes << 'editable' if controller.contentr_editable?
+            paragraph_classes << 'editable' if editable
             paragraph_options[:class] = paragraph_classes.join(' ')
 
             content_tag(:div, paragraph_options) do
-              render(:partial => "contentr/paragraphs/#{template_name}", :locals => {:paragraph => p})
+              s = ''.html_safe
+
+              if editable
+                s << content_tag(:div, :class => 'contentr toolbar') do
+                  'PARAGRAPH TOOLBAR'
+                end
+              end
+
+              s << render(:partial => "contentr/paragraphs/#{template_name}", :locals => {:paragraph => p})
             end
           end.join("").html_safe
         end
@@ -46,7 +63,7 @@ module Contentr
     def contentr_toolbar(options = {})
       if controller.contentr_editable?
         content_tag(:div, :class => 'contentr toolbar') do
-          link_to('#', 'mooo')
+          'CONTENTR TOOLBAR'
         end
       end
     end
