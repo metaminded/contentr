ecad798522177847319f395291fc13343c1d6eba
René Sprotte <rene.sprotte@provideal.net>
2011-05-29 20:28:59 +0200

    Secure the controller

--- app/controllers/contentr/admin/application_controller.rb
+++ app/controllers/contentr/admin/application_controller.rb
@@ -1,5 +1,11 @@
 class Contentr::Admin::ApplicationController < ApplicationController
 
+  before_filter :check_editable
+
   layout 'contentr_admin'
+
+  def check_editable
+    raise "Access Denied" unless contentr_editable?
+  end
 
 end
