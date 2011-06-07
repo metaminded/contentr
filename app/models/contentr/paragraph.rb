7754b27482acf2fe22614adffb910913714e0bbc
Peter Horn <peter.horn@provideal.net>
2011-06-07 22:54:26 +0200

    autgenerated paragraph forms

--- app/models/contentr/paragraph.rb
+++ app/models/contentr/paragraph.rb
@@ -23,6 +23,25 @@ module Contentr
         self.send "#{name}#{i}#{postfix}".to_sym
       end
     end
+    
+    def fields_for_simple_form
+      textpatt = /text|descr|content|body/
+      ul = {}
+      skipped = ["_id", "_type"]
+      if self.class.respond_to?(:uploaders)
+        ul = self.class.uploaders
+        skipped += self.class.uploader_options.map(&:last).map{|h| h[:mount_on]}
+      end
+      self.fields.map do |f| 
+        name = f.first
+        if skipped.member?(name) then nil else 
+          name = name.to_sym
+          options = {}
+          options[:as] = :file if ul[name]
+          [name, options]
+        end
+      end.compact
+    end
 
   end
 end
