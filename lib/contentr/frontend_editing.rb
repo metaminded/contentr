01cd3f9ebafe0a9a486037689809e7c7423a05cd
Florian Thomas <flo@florianthomas.net>
2012-10-08 13:55:57 +0200

    Add contentr_publisher helper

--- lib/contentr/frontend_editing.rb
+++ lib/contentr/frontend_editing.rb
@@ -4,11 +4,16 @@ module Contentr
 
     included do
       helper_method :contentr_authorized?
+      helper_method :contentr_publisher?
     end
 
     protected
 
     def contentr_authorized?
+      false
+    end
+
+    def contentr_publisher?
       false
     end
 
