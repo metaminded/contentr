3044223f8f13e4a10357a6e1dfd3a2332e43697b
Florian Thomas <flo@florianthomas.net>
2014-10-14 12:08:26 +0200

    applied patch from @hanspolo

--- app/controllers/contentr/admin/sub_pages_controller.rb
+++ app/controllers/contentr/admin/sub_pages_controller.rb
@@ -6,7 +6,7 @@ class Contentr::Admin::SubPagesController < Contentr::Admin::ApplicationControll
   end
 
   def reorder
-    nav_point_ids = params[:navpoints]
+    nav_point_ids = params[:ids].split(',')
     if nav_point_ids.present?
       non_removable_nav_points = Contentr::NavPoint.where(removable: false, parent_page_id: params[:id]).count
       nav_points = Contentr::NavPoint.where(parent_page_id: params[:id], removable: true).sort { |x,y| nav_point_ids.index(x.id.to_s) <=> nav_point_ids.index(y.id.to_s) }
