6189f956db8a34d9d075a5084a1bd6e8e3f235be
Florian Thomas <flo@florianthomas.net>
2015-03-02 18:36:09 +0100

    find only content pages in pages#index

--- app/controllers/contentr/pages_controller.rb
+++ app/controllers/contentr/pages_controller.rb
@@ -2,9 +2,10 @@ class Contentr::PagesController < Contentr::ApplicationController
   include Contentr::PagesControllerExtension
 
   def index
-    @area_containing_element = Contentr::Page.find_by(slug: params[:slug].split('_').last, language: I18n.locale.to_s)
+    @area_containing_element = Contentr::ContentPage.find_by(slug: params[:slug].split('_').last, language: I18n.locale.to_s)
+
     if @area_containing_element.nil? && I18n.locale != I18n.default_locale
-      @area_containing_element = Contentr::Page.find_by(slug: params[:slug].split('_').last, language: I18n.default_locale.to_s)
+      @area_containing_element = Contentr::ContentPage.find_by(slug: params[:slug].split('_').last, language: I18n.default_locale.to_s)
     end
     if @area_containing_element.present? && (@area_containing_element.viewable?(preview_mode: in_preview_mode?) || contentr_authorized?(type: :manage, object: @area_containing_element))
       @area_containing_element.preview! if in_preview_mode?
