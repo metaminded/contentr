c3002e96a08ce48e8a5d6734a59e36910c810b0f
Florian Thomas <flo@florianthomas.net>
2015-03-02 15:48:28 +0100

    use locale to find correct page for a given slug

--- app/controllers/contentr/pages_controller.rb
+++ app/controllers/contentr/pages_controller.rb
@@ -2,10 +2,13 @@ class Contentr::PagesController < Contentr::ApplicationController
   include Contentr::PagesControllerExtension
 
   def index
-    pages = Contentr::Page.where slug: params[:slug].split('_').last
-    @area_containing_element = pages.find{ |p| p.url.downcase == request.path.downcase }
+    @area_containing_element = Contentr::Page.find_by(slug: params[:slug].split('_').last, language: I18n.locale.to_s)
+    if @area_containing_element.nil? && I18n.locale != I18n.default_locale
+      @area_containing_element = Contentr::Page.find_by(slug: params[:slug].split('_').last, language: I18n.default_locale.to_s)
+    end
     if @area_containing_element.present? && (@area_containing_element.viewable?(preview_mode: in_preview_mode?) || contentr_authorized?(type: :manage, object: @area_containing_element))
       @area_containing_element.preview! if in_preview_mode?
+      flash.now[:notice] = t('contentr.content_not_available_in_language') if @area_containing_element.language != I18n.locale.to_s
       self.class.layout("layouts/#{Contentr.frontend_layout}")
     else
       raise ActionController::RoutingError.new('Not Found')
