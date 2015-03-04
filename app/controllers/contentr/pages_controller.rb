2e92e3baea114228a7c2dbf0bd7d55779f0acc1a
Philipp Hirsch <itself@hanspolo.net>
2015-03-04 20:45:03 +0100

    Find pages if the slug is used more than one time.

--- app/controllers/contentr/pages_controller.rb
+++ app/controllers/contentr/pages_controller.rb
@@ -2,11 +2,11 @@ class Contentr::PagesController < Contentr::ApplicationController
   include Contentr::PagesControllerExtension
 
   def index
-    @area_containing_element = Contentr::ContentPage.find_by(slug: params[:slug].split('_').last, language: I18n.locale.to_s)
-
-    if @area_containing_element.nil? && I18n.locale != I18n.default_locale
-      @area_containing_element = Contentr::ContentPage.find_by(slug: params[:slug].split('_').last, language: I18n.default_locale.to_s)
+    possible_pages = Contentr::ContentPage.where(slug: params[:slug].split('_').last, language: I18n.locale.to_s).to_a
+    if possible_pages.empty? && I18n.locale != I18n.default_locale
+      possible_pages = Contentr::ContentPage.where(slug: params[:slug].split('_').last, language: I18n.default_locale.to_s).to_a
     end
+    @area_containing_element = possible_pages.count == 1 ? possible_pages.first : possible_pages.find{|pp| pp.url_slug == params[:slug]}
     if @area_containing_element.present? && (@area_containing_element.viewable?(preview_mode: in_preview_mode?) || contentr_authorized?(type: :manage, object: @area_containing_element))
       @area_containing_element.preview! if in_preview_mode?
       flash.now[:notice] = t('contentr.content_not_available_in_language') if @area_containing_element.language != I18n.locale.to_s
