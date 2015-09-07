afd42012b7bba78375b1e58d6851302f8f69f5d6
Peter Horn <peter.horn@provideal.net>
2015-09-07 16:10:43 +0200

    removed deprecated tabulatr options

--- app/tabulatr_data/contentr/content_block_tabulatr_data.rb
+++ app/tabulatr_data/contentr/content_block_tabulatr_data.rb
@@ -2,15 +2,19 @@ class Contentr::ContentBlockTabulatrData < Tabulatr::Data
   buttons do |b, r|
     b.button :pencil, contentr.edit_admin_content_block_path(id: r.id)
     if r.usages.none?
-      b.button :times, contentr.admin_content_block_path(r.id), label: I18n.t('action.delete'), method: :delete, data: { confirm: I18n.t('action.are_you_sure')}
+      b.button :times, contentr.admin_content_block_path(r.id),
+        label: I18n.t('action.delete'), method: :delete,
+        data: { confirm: I18n.t('action.are_you_sure')}
     end
     unless r.partial.present?
       b.submenu do |s|
-        s.button :list, contentr.admin_area_paragraphs_path(record.class.name, record.id, 'main'), label: I18n.t('.contentr.content_block.paragraphs')
+        s.button :list, contentr.admin_area_paragraphs_path(record.class.name, record.id, 'main'),
+          label: I18n.t('.contentr.content_block.paragraphs')
       end
     end
   end
 
   column :name
-  association :usages, :count, table_column_options: {sortable: false, filter: false, header: I18n.t('.content_block.paragraphs.usages')}
+  association :usages, :count, sortable: false, filter: false,
+    header: I18n.t('.content_block.paragraphs.usages')
 end
